1549030c8e4589066b64302ecd5390cd3af8febf
arulkhair <amashdarulk@gmail.com>
2019-10-24 00:25:54 +0700

    Update imaaginatifTree

--- task2/imaginatifTree.js
+++ task2/imaginatifTree.js
@@ -2,8 +2,8 @@ function pohon(tahun_pohon,tinggi_pohon)
 {
 	for(let i = 0; i < tahun_pohon; i++)
 	{
-		// tinggi_pohon = tinggi_pohon * 2 + 1 + tinggi_pohon
-		tinggi_pohon = tinggi_pohon * 2 + 1
+		tinggi_pohon = tinggi_pohon * 2 + 1 + tinggi_pohon
+		// tinggi_pohon = tinggi_pohon * 2 + 1
 	}
 	console.log('tinggi akhir pohon',tinggi_pohon,'meter');
 	// console.log(tinggi_pohon);
